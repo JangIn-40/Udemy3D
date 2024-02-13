df0eb0553b20d23733bd09e1278577c1309fba0d
JangIn-40 <156999923+JangIn-40@users.noreply.github.com>
2024-02-13 17:37:09 +0900

    Udemy3D 따라해봄
    버전달라서 cinemachine부분 약간씩 다름

---
+++ Assets/Scripts/Mover.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class Mover : MonoBehaviour
+{
+    [SerializeField] float moveSpeed = 10f;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        float xValue = Input.GetAxis("Horizontal") * moveSpeed * Time.deltaTime;
+        float zValue = Input.GetAxis("Vertical") * moveSpeed * Time.deltaTime;
+        transform.Translate(xValue, 0, zValue);
+    }
+}
